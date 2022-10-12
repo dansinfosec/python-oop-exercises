fa30c61220c52b28ab9a95d7199564c744f5d73f
dansinfosec <danberger08@gmail.com>
2022-10-12 10:45:12 +0100

    pygame and gui extra comments

--- chapter05/Pygame/PygameDemo0_WindowOnly/PygameWindowOnly.py
+++ chapter05/Pygame/PygameDemo0_WindowOnly/PygameWindowOnly.py
@@ -29,14 +29,32 @@ while True:
             pygame.quit()
             sys.exit()
         # 8 - Do any "per frame" actions
+        # In this part we eventually check for let's say colliding elements
+        # any code you want to run in the frame
 
         # 9 - Clear the window
+        # On each iteration through the loop, our program must redraw everything
+        # We can use a background image or whatever but we use a black background for now
         window.fill(BLACK)
 
         # 10 - Draw all window elements
+        # Here we will place code to draw everything we want to show in our window
+        # In real programs things are drawn in the order they appear in the code,
+        # in layers from backmost to frontmost Natural mapping equivalent to Photoshop
 
         # 11 - Update the window
+        # This line tells pygame to take all the drawing we've included and show it in the window.
+        # When you tell pygame to update it takes the contents of this off-screen buffer created in 8,9
+        # And puts them in the real window
+
         pygame.display.update()
 
         # 12 - Slow things down a bit
+        # Computers are very fast, and if the loop continnued to the next iteration right away without pausing
+        # the program might run faster than the desired framerate
+        # The line in this section tellls pygame to wait untill a given amount of time has elapsed
+        # in order to make the frames of our program run at the framerate we specified
+        # This is important to ensure our program runs at a consistent rate,
+        # independent of the speed of the computer on which it's running
+
         clock.tick(FRAMES_PER_SECOND)
