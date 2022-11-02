3d8534d7b5b5157ba9bc012c539be95353b68639
dansinfosec <danberger08@gmail.com>
2022-11-02 12:45:30 +0100

    pygame

--- chapter05/Pygame/PygameDemo1_ImageOnly/PygameOneImage.py
+++ chapter05/Pygame/PygameDemo1_ImageOnly/PygameOneImage.py
@@ -1,8 +1,10 @@
 # pygame demo 0 - window only
 # 1 - Import packages
+
 import pygame
 from pygame.locals import *
 import sys
+
 
 # 2 - Define constants
 BLACK = (0, 0, 0)
@@ -10,18 +12,13 @@ WINDOW_WIDTH = 640
 WINDOW_HEIGHT = 480
 FRAMES_PER_SECOND = 30
 
-# 2.1 - Add path for image files
-BASE_PATH = Path(__file__).resolve().parent
-# - ---- Build a path to the file in the images folder
-pathToBall = BASE_PATH + 'images/ball.png'
-
-
 # 3 - Initialize the world
 pygame.init()
 window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 clock = pygame.time.Clock()
 
 # 4 - Load assets: image(s), sound(s), etc.
+ballImage = pygame.image.load('images/ball.png')
 
 # 5 - Initialize variables
 
@@ -47,6 +44,8 @@ while True:
         # Here we will place code to draw everything we want to show in our window
         # In real programs things are drawn in the order they appear in the code,
         # in layers from backmost to frontmost Natural mapping equivalent to Photoshop
+        # - Drawball at position 100 accross (x) and 200 down(y)
+        window.blit(ballImage, (100, 200))
 
         # 11 - Update the window
         # This line tells pygame to take all the drawing we've included and show it in the window.
